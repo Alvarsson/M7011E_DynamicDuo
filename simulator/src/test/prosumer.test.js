040ad85036ea40ae88dfd5cbdeac3d6140a95537
Alvarsson <a-alvarsson@hotmail.com>
2020-11-23 16:39:38 +0100

    gonna stash

--- simulator/src/test/prosumer.test.js
+++ simulator/src/test/prosumer.test.js
@@ -1,6 +1,4 @@
 var assert = require('assert');
-//var expect = require('mocha').expect;
-//var prosumer = require('simulator/src/entities/Prosumer.js');
 const Prosumer = require('../entities/Prosumer');
 
 describe('Prosumer', function() {
@@ -8,11 +6,11 @@ describe('Prosumer', function() {
 
   describe('Get prosumer id', function() {
     it('should return prosumer id 0.', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-      //var proId = prosumer.get_prosumer_id();
-      //expect(proId).to.equal(0);
+      assert.equal(prosumer.get_prosumer_id(), 0);
     });
   });
+
+  //describe('')
 
 
 });
