d3ac4426e74a5df80d158903d53585e2a6fdf157
Alvarsson <a-alvarsson@hotmail.com>
2020-12-02 14:16:19 +0100

    need to rebase once again

---
+++ rest/api/models/manager_log_model.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ManagerLogSchema = new Schema({
+    id: {
+        type: String,
+        Unique: false,
+        Required: "Need a manager ID"
+    },
+    market_price: {
+        type: Number
+    },
+    battery_level: {
+        type: Number
+    },
+    production: {
+        type: Number
+    },
+    tick: {
+        type: Number
+    },
+    total_net_consumption: {
+        type: Number
+    },
+    power_plant_consumption: {
+        type: Number
+    },
+    nr_of_consumers: {
+        type: Number
+    }
+});
+
+module.exports = mongoose.module('ManagerLog', ManagerLogSchema);
