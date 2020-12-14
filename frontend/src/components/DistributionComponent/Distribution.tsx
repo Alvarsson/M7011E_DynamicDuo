3ea9bf8033e74a0c75d060cb23af75555dafa714
Albin Thomsson <albtho-5@student.ltu.se>
2020-12-14 04:43:56 +0100

    moved some files

---
+++ frontend/src/components/DistributionComponent/Distribution.tsx
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import Container from "react-bootstrap/Container";
+
+import LabelledSlider from "./LabelledSlider";
+import Button from "react-bootstrap/Button";
+
+import Card from "react-bootstrap/esm/Card";
+import {CURRENTUSER} from '../../constants/apiConstants';
+
+interface Props {
+  initialValueA: number;
+  initialValueB: number;
+}
+
+
+
+const Distribution: React.FC<Props> = ({ initialValueA, initialValueB }) => {
+  const [valA, setValA] = useState(initialValueA);
+
+  const [valB, setValB] = useState(initialValueB);
+
+  function handleA(value: number) {
+    //kanske kan plocka in id här och meka.. vem vet. inte såhär dock...
+    console.log(Math.round(value));
+    setValA(Math.round(value));
+    return null;
+  }
+
+  function handleB(value: number) {
+    //kanske kan plocka in id här och meka.. vem vet. inte såhär dock...
+    setValB(Math.round(value));
+    return null;
+  }
+
+  function postChanges() {
+    postDistribution_Over();
+    postDistribution_under(); //testar bara att de funkar. kanske ska göras till async/await eller göras om helt
+  }
+
+  function postDistribution_Over() {
+
+    return axios.put(
+      "http://localhost:3001/prosumersettings/"+ localStorage.getItem(CURRENTUSER)+"/distr_over",
+      {
+        distribution: {
+          sell: (100 - valA) / 100,
+          store: valA / 100,
+        },
+      },
+      
+    );
+  }
+
+  function postDistribution_under() {
+    return axios
+      .put(
+        "http://localhost:3001/prosumersettings/"+ localStorage.getItem(CURRENTUSER)+"/distr_under",
+        {
+          distribution: {
+            buy: (100 - valB) / 100,
+            drain: valB / 100,
+          },
+        },
+        
+      )
+      .then((response) => console.log(response));
+  }
+
+  return (
+    <Container className="p-3">
+      <Card style={{ width: "50vw" }} bg="light">
+        <Card.Body>
+          <Card.Title>{"For over-production:"}</Card.Title>
+
+          <LabelledSlider
+            labelLeft={"Sell"}
+            labelRight={"Store"}
+            defaultValue={valA}
+            callback={handleA}
+          />
+          <h4>{"For under-production:"}</h4>
+          <LabelledSlider
+            labelLeft={"Buy"}
+            labelRight={"Drain"}
+            defaultValue={valB}
+            callback={handleB}
+          />
+
+          <Button onClick={postChanges}> Save </Button>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+};
+
+export default Distribution;
---
+++ frontend/src/components/logsComponent/DashSimple.js
@@ -0,0 +1,159 @@
+//@ts-ignore
+
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import DashComponent from "./DashComponent";
+import Col from "react-bootstrap/esm/Col";
+import Row from "react-bootstrap/esm/Row";
+import CardDeck from "react-bootstrap/CardDeck";
+
+import { API_BASE_URL, CURRENTUSER } from "../../constants/apiConstants";
+
+export default function DashSimple() {
+  //const [data, setData] = useState("");
+
+  const initialStateArray = [
+    { tick: -4, value: 0 },
+    { tick: -3, value: 0 },
+    { tick: -2, value: 0 },
+    { tick: -1, value: 0 },
+    { tick: 0, value: 0 },
+  ];
+  const [a, setA] = useState(0);
+  const [tick, setTick] = useState(0);
+  const [curr, setCurrent] = useState([0, 0, 0]);
+  const [productionArray, setProductionArray] = useState(initialStateArray);
+  const [consumptionArray, setConsumptionArray] = useState(initialStateArray);
+  const [windArray, setWindArray] = useState(initialStateArray);
+
+  const dataLimit = 9;
+
+  const fetchData = (limit) => {
+    const url =
+      API_BASE_URL +
+      "/prosumerlog/" +
+      localStorage.getItem(CURRENTUSER) +
+      "/getlatest/" +
+      limit;
+
+    return axios
+      .get(url)
+      .then(({ data }) => {
+        return data;
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const setStates = (inData) => {
+    setProductionArray((oldArray) => {
+      if (oldArray.length > dataLimit) {
+        oldArray.shift();
+      }
+      return [...oldArray, { tick: inData.tick, value: inData.production }];
+    });
+
+    setConsumptionArray((oldArray) => {
+      if (oldArray.length > dataLimit) {
+        oldArray.shift();
+      }
+      return [...oldArray, { tick: inData.tick, value: inData.consumption }];
+    });
+
+    setWindArray((oldArray) => {
+      if (oldArray.length > dataLimit) {
+        oldArray.shift();
+      }
+      return [
+        ...oldArray,
+        { tick: inData.tick, value: inData.weather.wind_speed },
+      ];
+    });
+  };
+
+  const initializeData = (latestLogs) => {
+    console.log("init data");
+
+    if (typeof latestLogs[0] === "undefined") {
+      console.log("latest logs undefined");
+      setCurrent([0, 0, 0]);
+      //setStates([]);
+      return;
+    }
+    setCurrent([
+      latestLogs[0].production,
+      latestLogs[0].consumption,
+      latestLogs[0].weather.wind_speed,
+    ]);
+    
+    latestLogs.reverse().forEach((element) => {
+      //måste bli bättre än såhär på något sätt, right?
+      setStates(element);
+    });
+  };
+
+  const updateData = (response) => {
+    var res = response[0];
+
+    try {
+      setTick(res.tick);
+
+      if (res.tick === tick) {
+        //same tick? dont change anythin.
+        return null;
+      } else {
+        setCurrent([res.production, res.consumption, res.weather.wind_speed]);
+
+        setStates(res);
+      }
+    } catch (error) {
+      console.log("no tick");
+    }
+  };
+
+  useEffect(() => {
+    // maybe move to moveTimeout?
+    if (a !== 1) {
+      fetchData(dataLimit+1).then((latestLogs) => {
+        initializeData(latestLogs);
+      });
+      setA(1);
+    }
+    const interval = setInterval(() => {
+      fetchData(1).then((res) => {
+        updateData(res);
+      });
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [tick]); //actually update states when the tick has changed.
+
+  return (
+    <Container className="fluid">
+      <Row>
+        <CardDeck>
+          <DashComponent
+            dataType={"Production"}
+            display={productionArray}
+            current={curr[0]}
+            strokeColor={"#4655f5"}
+          />
+          <DashComponent
+            dataType={"Consumption"}
+            display={consumptionArray}
+            current={curr[1]}
+            strokeColor={"#1edc00"}
+          />
+          <DashComponent
+            dataType={"Windspeed"}
+            display={windArray}
+            current={curr[2]}
+            strokeColor={"#ff3c28"}
+          />
+        </CardDeck>
+      </Row>
+    </Container>
+  );
+}
