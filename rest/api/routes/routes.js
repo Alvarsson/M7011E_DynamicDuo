7784df8ef3aa87d77fcbd51cc2e5b923e9dc3768
Albin Thomsson <albtho-5@student.ltu.se>
2020-12-15 04:15:10 +0100

    commented out the auth part of the routes for the presentation

--- rest/api/routes/routes.js
+++ rest/api/routes/routes.js
@@ -8,13 +8,13 @@ module.exports = function(app) {
 	var wind_speed = require('../controllers/windSpeedController');
 	var blackout = require('../controllers/blackoutController');
 	const { RegisterUser, LoginUser, LogoutUser, getUserDetails } = require('../controllers/authController');
-	const { auth } = require('../middleware/auth');
+	//const { auth } = require('../middleware/auth');
 	
 	//  If we want to use several files with just the route variable name, we could create the index file and export from there.
 	
 // TODO:
 // - prosumer setting should be created when registering a prosumer
-// - auth protocol to get prosumer settings and rest
+// - /*auth*/ protocol to get prosumer settings and rest
 
 
 	// --------- PROSUMER ---------
@@ -25,22 +25,22 @@ module.exports = function(app) {
 		.post(prosumer.add_prosumer_setting); // add new prosumer setting
 
 	app.route('/prosumersettings/:id')
-		.get(auth,prosumer.get_prosumer_setting);     //Get prosumer setting for id
+		.get(/*auth*,*/prosumer.get_prosumer_setting);     //Get prosumer setting for id
 	app.route('/prosumersettings/:id/img_url')
-		.put(auth,prosumer.update_prosumer_settings_img_url); // update img_url for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_img_url); // update img_url for id
 	app.route('/prosumersettings/:id/password')
-		.put(auth,prosumer.update_prosumer_settings_password); // update password for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_password); // update password for id
 	app.route('/prosumersettings/:id/online')
-		.put(auth,prosumer.update_prosumer_settings_online); // update online for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_online); // update online for id
 	app.route('/prosumersettings/:id/distr_over')
-		.put(auth,prosumer.update_prosumer_settings_distr_over); // update over distr for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_distr_over); // update over distr for id
 	app.route('/prosumersettings/:id/distr_under')
-		.put(auth,prosumer.update_prosumer_settings_distr_under); // update under distr for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_distr_under); // update under distr for id
 	app.route('/prosumersettings/:id/battery_warning_threshold')
-		.put(auth,prosumer.update_prosumer_settings_battery_warning_threshold); // update battery warn threshold for id
+		.put(/*auth*,*/prosumer.update_prosumer_settings_battery_warning_threshold); // update battery warn threshold for id
 	
 	app.route('/prosumersettings/:id/delete') // delete the specific prosumer  settings
-		.delete(auth,prosumer.delete_prosumer_settings);
+		.delete(/*auth*,*/prosumer.delete_prosumer_settings);
 	
 
 	// ---- log ----
@@ -49,7 +49,7 @@ module.exports = function(app) {
 	app.route('/prosumerlog/:id/getall') // get all specific prosumer logs
 		.get(prosumer_log.get_all_prosumer_logs);
 	app.route('/prosumerlog/:id/getlatest/:limit?') // get latest specific prosumer log, with optional limiter
-		.get(auth,prosumer_log.get_latest_prosumer_log);
+		.get(/*auth*,*/prosumer_log.get_latest_prosumer_log);
 	app.route('/prosumerlog/:id/delete') // delete all specific prosumer logs
 		.delete(prosumer_log.delete_prosumer_logs);
 	
@@ -62,17 +62,17 @@ module.exports = function(app) {
 		.post(manager.add_manager_setting);
 	
 	app.route('/managersettings/get') // get the manager settings
-		.get(auth, manager.get_manager_setting);	
+		.get(/*auth*,*/ manager.get_manager_setting);	
 	app.route('/managersettings/img_url') // set the manager img_url
-		.put(auth, manager.update_manager_setting_img_url);
+		.put(/*auth*,*/ manager.update_manager_setting_img_url);
 	app.route('/managersettings/password') // set the manager password
-		.put(auth, manager.update_manager_setting_password);
+		.put(/*auth*,*/ manager.update_manager_setting_password);
 	app.route('/managersettings/online') // set the manager online number
-		.put(auth, manager.update_manager_settings_online);
+		.put(/*auth*,*/ manager.update_manager_settings_online);
 	app.route('/managersettings/battery_warning_threshold') // set the manager battery threshold warning
-		.put(auth, manager.update_manager_settings_battery_warning_threshold);
+		.put(/*auth*,*/ manager.update_manager_settings_battery_warning_threshold);
 	app.route('/managersettings/delete') // delete the manager settings
-		.delete(auth, manager.delete_manager_settings);
+		.delete(/*auth*,*/ manager.delete_manager_settings);
 
 	// ---- log ----
 	app.route('/managerlog') // add a manager log
@@ -109,11 +109,11 @@ module.exports = function(app) {
 	app.route('/login')
 		.post(LoginUser);
 	app.route('/logout')
-		.get(auth, LogoutUser);
+		.get(/*auth*,*/ LogoutUser);
 	app.route('/register')
 		.post(RegisterUser);
 	app.route('/details')
-		.get(auth, getUserDetails);
+		.get(/*auth*,*/ getUserDetails);
 
 };
 
